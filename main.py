a61b28b93ed66120012d541b24d082c4314fcbee
Norbert Gocze <norbert.gocze@evotrex.eu>
2021-12-06 10:30:27 +0100

    Implement main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello there")
+#!/usr/bin/env python3
+from flask import Flask
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run(
+        debug=False,
+        host='127.0.0.1',
+        port=8080
+    )
