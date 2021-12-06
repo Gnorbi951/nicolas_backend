cdc02f5a700fc6ab458c1f6fe4b7c73aa54874c5
Norbert Gocze <norbert.gocze@evotrex.eu>
2021-12-06 11:29:37 +0100

    Add initdb

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 from flask import Flask
 from router.router import main_router
+from db.connection import initdb
 
 app = Flask(__name__)
 app.register_blueprint(main_router)
 
 if __name__ == '__main__':
+    initdb()
     app.run(
         debug=False,
         host='127.0.0.1',
